da4be0721380fbb0c629e5692f30c151acadf55f
Vignnite <102588121+Vignnite@users.noreply.github.com>
2023-01-17 15:20:13 +0530

    commit before cont

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const l = document.getElementById("lem");
 window.addEventListener("scroll", function () {
   let offset = window.pageYOffset;
   // console.log("Offset : " + offset);
-  l.style.backgroundPositionY = offset * -0.225 + "px";
+  l.style.backgroundPositionY = offset * -0.175 + "px";
 });

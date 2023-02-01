f4b5ad32086b73af91b922236c9f7d7606e7808a
Vignnite <102588121+Vignnite@users.noreply.github.com>
2023-02-01 23:51:55 +0530

    post_js

--- signin.js
+++ signin.js
@@ -31,3 +31,19 @@ function validate() {
 //   let p = document.getElementsByClassName("log").value;
 //   console.log(p);
 // }
+function textfoc(j) {
+  m = document.getElementById("un");
+  m.style["width"] = "80%";
+}
+function textblur(k) {
+  m = document.getElementById("un");
+  m.style["width"] = "70%";
+}
+function mmove(l) {
+  l.style.color = "blue";
+  l.style.textShadow = "2px 2px 10px 7px rgb(255, 216, 216)";
+}
+function mmout(o) {
+  o.style.color = "orange";
+  o.style.textShadow = "rgb(10, 40, 142)";
+}
